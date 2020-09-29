86e702d1615808746a08fe505f80aa90e84eab2b
Gaurav Singh <gauravsingh@circuitvalley.com>
2020-09-30 00:26:58 +0200

    byte aligner reset generator delays reset to byte aligner to avoid spurious bytes when LP to HS

---
+++ FPGA/byte_aligner_reset_generator.v
@@ -0,0 +1,32 @@
+`timescale 1ns/1ns
+
+
+module byte_aligner_reset_generator(clk_i,
+									reset_i,
+									aligner_reset_o);
+input clk_i;
+input reset_i;
+output reg aligner_reset_o;
+
+reg [3:0]clk_count;
+
+always @(negedge clk_i)
+begin
+	if (reset_i)
+	begin
+		aligner_reset_o <= 1;
+		clk_count  <=4'h0;
+	end
+	else
+	begin
+		clk_count <= clk_count + 1'b1;
+		
+		if (clk_count == 4'd2)
+		begin
+			aligner_reset_o <= 1'b0;
+		end
+
+	end
+end
+
+endmodule
